ea294513cac61d6cdc618537bdea6416164252b4
Sagar Daswani <sagardaswani401@gmail.com>
2020-05-25 11:08:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Healthcare_Startup_Assessment
+ Impact of Social Media on crowd funding campaigns
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Social Media is crucial to the success of crowd funding campaigns for individuals, brands and non-profits. Focus on creating shareable content and cultivating a community on social media so one can reach new audiences. The more effectively you use social media, the more contributions, pledges and donations one will recieve',
+    author='Northeastern_University-Team_Alpha',
+    license='',
+)
